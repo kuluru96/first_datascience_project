5cf6fe80a268b3f23c9cdcebf2c518871685e8ea
kuluru96 <muralidhark36@gmail.com>
2021-02-01 19:27:59 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First steps into Data Scince with Titanic Dataset',
+    author='kuluru96',
+    license='MIT',
+)
